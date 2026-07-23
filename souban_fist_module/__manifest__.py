1105adb6612eeaac503b26cea3da1abe1d946278
Muhammed Souban <souban@gmail.com>
2026-07-23 21:35:52 +0530

    update version to 19.0.0.2 in manifest

--- souban_fist_module/__manifest__.py
+++ souban_fist_module/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Training may 2026',
-    'version': '19.0.0.1',
+    'version': '19.0.0.2',
     'summary': 'This Student model',
     'description': """This is student model """,
     'category': 'training',
